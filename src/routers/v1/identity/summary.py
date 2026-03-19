499154a3b26dc8a48b44e1915ff94b9e0bbad926
Eliaveta88 <222729@edu.fa.ru>
2026-03-19 15:58:54 +0300

    Update src/routers/v1/identity/summary.py.
    Apply focused improvement for Redis workflow and related logic in this file.

--- src/routers/v1/identity/summary.py
+++ src/routers/v1/identity/summary.py
@@ -2,5 +2,6 @@
 
 LOGIN_SUMMARY = "User login"
 LOGOUT_SUMMARY = "User logout"
+LOGOUT_ALL_SUMMARY = "Logout user from all sessions"
 GET_CURRENT_USER_SUMMARY = "Get current user"
 CREATE_USER_SUMMARY = "Create user"
